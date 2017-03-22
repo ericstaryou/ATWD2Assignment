0a46517d156d096bc0c4892ddbc8a1ae17a24534
Eric Yew <eric9yew@gmail.com>
2017-03-22 14:30:32 +0000

    comment added

--- DateDropdown.php
+++ DateDropdown.php
@@ -1,6 +1,5 @@
 <?php 
 	$station = htmlspecialchars($_GET["station"]);
-	//$station = "Fishponds";
   	$loc_name = str_replace(' ', '_', $station);
   	$file_name = strtolower($loc_name).'_no2.xml';
 	
@@ -12,22 +11,22 @@
   	while($reader->read()){
   		if($reader->nodeType === XMLREADER::ELEMENT && $reader->localName === 'reading'){
   			$reading = array();
-  			//array_push($data, $reader->getAttribute('date'));
   			$date = $reader->getAttribute('date');
   			$dateForSort = str_replace('/', '-', $date);
-        	$reading['sortdate'] = date('Y-m-d', strtotime($dateForSort));
+        		$reading['sortdate'] = date('Y-m-d', strtotime($dateForSort));
   			$reading['date'] = $date;
   			$data[] = $reading;
   		}
   	}
 
-  	//remove duplicate values from a multi-dimensional array adopted from: 
-  	//http://stackoverflow.com/questions/307674/how-to-remove-duplicate-values-from-a-multi-dimensional-array-in-php
+  	//remove duplicate values from a multi-dimensional array 
+  	//ref: http://stackoverflow.com/questions/307674/how-to-remove-duplicate-values-from-a-multi-dimensional-array-in-php
   	$data = array_map("unserialize", array_unique(array_map("serialize", $data)));
-
-  	//$result = array_unique($data);
+	
+	//sort the data according to the date
   	asort($data);
-  	//print_r($data);
+
+	//generate date dropdown options 
   	$option = null;
 	foreach($data as $date){
 		if($option == null){
@@ -36,6 +35,6 @@
 			$option = $option . '<option value="' . $date['date'] . '">' . $date['date'] . '</option>';
 		}
 	}
-	//echo '<select name="date" id="dateform" onchange="">' . $option . '</select>';
+	
 	echo $option;
 ?>
